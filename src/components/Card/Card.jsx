48bc20e63fda5427c6b1a10307e5e845bc4a8117
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2022-06-18 17:47:36 +0300

    + card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
+import { API_IMAGE_URL } from '../../constants';
+
+function Card({ item, onClick }) {
+  return (
+    <li className={styles.card}>
+      <div className={styles.inner}>
+        <div className={styles.reactions}>
+          <button type="button" className={styles.like} title="Like">
+            &#128077;
+          </button>
+          <button type="button" className={styles.dislike} title="Dislike">
+            &#128078;
+          </button>
+          <span className={styles.likes}>Likes</span>
+          <hr />
+          <span className={styles.count}>0</span>
+        </div>
+        <div className={styles.info}>
+          <h2 onClick={onClick} aria-hidden="true" id={item.id}>{item.title}</h2>
+          <div className={styles.poster}>
+            <img src={`${API_IMAGE_URL}${item.backdrop_path}`} alt={item.title} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        {item.vote_average}
+      </div>
+    </li>
+  );
+}
+
+export default Card;
+
+Card.propTypes = {
+  item: PropTypes.shape({
+    adult: PropTypes.bool,
+    backdrop_path: PropTypes.string,
+    genre_ids: PropTypes.arrayOf(PropTypes.number),
+    id: PropTypes.number,
+    original_language: PropTypes.string,
+    original_title: PropTypes.string,
+    overview: PropTypes.string,
+    popularity: PropTypes.number,
+    poster_path: PropTypes.string,
+    release_date: PropTypes.string,
+    title: PropTypes.string,
+    video: PropTypes.bool,
+    vote_average: PropTypes.number,
+    vote_count: PropTypes.number,
+  }),
+  onClick: PropTypes.func.isRequired,
+};
+
+Card.defaultProps = {
+  item: {},
+};
