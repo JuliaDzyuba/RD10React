86915af1a2860dbd404c875686121ff1d8cb4341
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2022-06-26 09:29:29 +0300

    * added routes for movie detail page and actor page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Info from '../components/Info';
+import ActorInfoPage from '../pages/ActorInfoPage';
 import MainPage from '../pages/MainPage';
 
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/">
-        <MainPage />
-      </Route>
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/movies/:movieId" component={Info} />
+      <Route exact path="/actor/:actorId" component={ActorInfoPage} />
       <Route path="*">
         <h1 style={{ marginBottom: 'auto' }}>Not found</h1>
       </Route>
