baf98abcb52ee865fd3a8f0de555571a0a7896fd
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2022-07-02 10:15:39 +0300

    + routs constants

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,17 +9,18 @@ import NotFound from '../pages/NotFound';
 import Registration from '../pages/Registration/Registration';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+import * as appRoutes from './routesConstants';
 
 function Routes() {
   return (
     <Switch>
-      <PrivateRoute exact path="/movies/:movieId" component={Info} />
-      <PrivateRoute exact path="/actor/:actorId" component={ActorInfoPage} />
-      <PublicRoute exact path="/login" component={Login} />
-      <PublicRoute exact path="/registration" component={Registration} />
-      <PrivateRoute exact path="/" component={MainPage} />
-      <PrivateRoute exact path="/movies/:movieId/edit" component={EditMovie} />
-      <Route path="/*" component={NotFound} />
+      <PrivateRoute exact path={appRoutes.MOVIE_ID} component={Info} />
+      <PrivateRoute exact path={appRoutes.ACTOR_ID} component={ActorInfoPage} />
+      <PublicRoute exact path={appRoutes.LOGIN} component={Login} />
+      <PublicRoute exact path={appRoutes.REGISTRATION} component={Registration} />
+      <PrivateRoute exact path={appRoutes.ROOT} component={MainPage} />
+      <PrivateRoute exact path={appRoutes.MOVIE_ID_EDIT} component={EditMovie} />
+      <Route path={appRoutes.ANY} component={NotFound} />
     </Switch>
   );
 }
