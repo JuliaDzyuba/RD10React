78d5b4c78c36647c284ffec788d3f0980f65e247
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2022-07-03 19:42:20 +0300

    * used formik for edit movie form

--- src/pages/EditMovie/EditMovie.jsx
+++ src/pages/EditMovie/EditMovie.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory, useParams } from 'react-router-dom';
+import { useFormik } from 'formik';
 import useTranslation from '../../hooks/useTranslation';
+import validateForm from '../../utils/validateForm';
 import { editMovie } from '../../store/slices/movie.slice';
 import styles from './styles.module.scss';
 
@@ -14,77 +16,78 @@ function EditMovie() {
   const { lang } = useSelector((state) => state.userReducer);
   const intl = useTranslation(lang);
 
-  const [formValue, setFormValue] = useState({
-    title: currentMovie.title,
-    backdrop_path: currentMovie.backdrop_path,
-    genres: currentMovie.genres.map((g) => g.name).join(','),
-    overview: currentMovie.overview,
+  const formik = useFormik({
+    initialValues: {
+      title: currentMovie.title,
+      backdrop_path: currentMovie.backdrop_path,
+      genres: currentMovie.genres.map((g) => g.name).join(','),
+      overview: currentMovie.overview,
+    },
+    validate: validateForm,
+    onSubmit: (formValue) => {
+      dispatch(editMovie({ movieId, formValue }));
+      history.goBack();
+    },
   });
-
-  const handleSubmit = (e) => {
-    e.prevent.default();
-  };
-
-  const handleChange = (e) => {
-    setFormValue({ ...formValue, [e.target.name]: e.target.value });
-  };
-
-  const handleClick = () => {
-    dispatch(editMovie({ movieId, formValue }));
-    history.goBack();
-  };
 
   return (
     <div className={styles.container}>
       <h1>{intl['app-editpage']}</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="title">
           {intl['app-editpage-title']}
+          {formik.errors.title ? <p className={styles.error}>{formik.errors.title}</p> : null}
           <input
             type="text"
             name="title"
             id="title"
             placeholder={intl['app-editpage-title-placeholder']}
-            value={formValue.title}
-            onChange={handleChange}
+            value={formik.values.title}
+            onChange={formik.handleChange}
           />
         </label>
         <label htmlFor="imageUrl">
           {intl['app-editpage-image']}
+          {formik.errors.backdrop_path
+          && <p className={styles.error}>{formik.errors.backdrop_path}</p>}
           <input
             type="text"
             name="backdrop_path"
             id="imageUrl"
             placeholder={intl['app-editpage-image-placeholder']}
-            value={formValue.backdrop_path}
-            onChange={handleChange}
+            value={formik.values.backdrop_path}
+            onChange={formik.handleChange}
           />
         </label>
         <label htmlFor="genres">
           {intl['app-editpage-genres']}
+          {formik.errors.genres
+          && <p className={styles.error}>{formik.errors.genres}</p>}
           <input
             type="text"
             name="genres"
             id="genres"
             placeholder={intl['app-editpage-genres-placeholder']}
-            value={formValue.genres}
-            onChange={handleChange}
+            value={formik.values.genres}
+            onChange={formik.handleChange}
           />
         </label>
         <label htmlFor="description">
           {intl['app-editpage-description']}
+          {formik.errors.overview
+          && <p className={styles.error}>{formik.errors.overview}</p>}
           <textarea
             type="text"
             name="overview"
             id="description"
             placeholder={intl['app-editpage-description-placeholder']}
-            value={formValue.overview}
-            onChange={handleChange}
+            value={formik.values.overview}
+            onChange={formik.handleChange}
             rows={10}
           />
         </label>
         <div className={styles.buttonsGroup}>
-          <button type="button" className={styles.btn} onClick={handleClick}>{intl['app-editpage-button-save']}</button>
+          <button type="submit" className={styles.btn}>{intl['app-editpage-button-save']}</button>
           <Link to={`/movies/${movieId}`} className={styles.btn}>{intl['app-editpage-button-back']}</Link>
         </div>
       </form>
