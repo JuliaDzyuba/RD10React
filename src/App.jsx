762dc18531273bcfcbe70c6f8a02186af97e1120
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2022-11-11 13:05:52 +0200

    added hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMoviesList } from './store/actions/actions';
 
